f7f6c75f6ef6415c29541a6416c5664ab63556e6
Abdulaziz Alobaili <dr.alobaili@gmail.com>
2019-01-21 17:01:08 +0300

    Add TODOs

---
+++ MedicalRecord.sol
@@ -0,0 +1,27 @@
+// TODO: Constructor
+
+// TODO: addEmergencyContact
+
+// TODO: getEmergencyContacts
+
+// TODO: addDiognosis
+
+// TODO: getDiognosis
+
+// TODO: addDrugPrescription
+
+// TODO: getDrugPrescription
+
+// TODO: addSurgery
+
+// TODO: getSurgeries
+
+// TODO: addLabTest
+
+// TODO: getLabTest
+
+// TODO: addBloodDonation
+
+// TODO: getBloodDonation
+
+// TODO: markTransactionAsCorrection
---
+++ MedicalRecordsSystem.sol
@@ -0,0 +1,11 @@
+// TODO: Constructor
+
+// TODO: createMedicalRecord
+
+// TODO: checkMedicalRecord
+
+// TODO: getMedicalRecord
+
+// TODO: addHospital
+
+// TODO: addPharmacy
