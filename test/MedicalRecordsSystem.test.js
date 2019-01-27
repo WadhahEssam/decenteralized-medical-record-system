bc5b75dd19c27f88fc59a0fb377536c1bd04b921
Wadhah Essam Mac <wadah-wleed@hotmail.com>
2019-01-28 02:21:13 +0300

    adding test for adding a hospital

--- test/MedicalRecordsSystem.test.js
+++ test/MedicalRecordsSystem.test.js
@@ -12,14 +12,23 @@ let ministryOfHelath, hospitalOne, hospitalTwo, pharmacyOne, pharmacyTwo;
 
 beforeEach( async () => {
   accounts = await web3.eth.getAccounts();
+  ministryOfHelath = accounts[0];
+  hospitalOne = accounts[1];
+  hospitalTwo = accounts[2];
 
   medicalRecordsSystemContract = await new web3.eth.Contract(JSON.parse(compiledMedicalRecordsSystem.interface))
     .deploy({ data: compiledMedicalRecordsSystem.bytecode })
-    .send({ from: accounts[0], gas: '1000000' });
+    .send({ from: ministryOfHelath, gas: '1000000' });
 });
 
 describe('MedicalRecordSystem Contract', async () => {
   it('is deployed and has address', async () => {
-    assert.notEqual('1','0101');
+    assert.ok(medicalRecordsSystemContract.options.address);
+  });
+
+  it('minisity of health can add hospital', async () => {
+    await medicalRecordsSystemContract.methods.addHospital(hospitalOne).send({ from: ministryOfHelath, gas: '1000000' });
+    const hospitalAddress = await medicalRecordsSystemContract.methods.hospitalAddresses(0).call();
+    assert.equal(hospitalOne, hospitalAddress);
   });
 }); 
