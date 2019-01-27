e5c1b287bb7b41941bc165af7f9b35827f2c6dc5
abdo1236 <homsillusion@gmail.com>
2019-01-27 08:20:39 +0300

    Update MedicalRecord.sol

--- contracts/MedicalRecord.sol
+++ contracts/MedicalRecord.sol
@@ -1,28 +1,147 @@
+pragma solidity ^0.5.0;
 
-// TODO: Constructor
+contract medicalRecord{
+    
+    
+    address public medicalRecordAddress;
+    string public nationalID;
+    string public name;
+    //Date  
+    string public phoneNumber;
+    string public gender;
+    string public bloodType;
+    string[] public emergencyContants;
+    Diagnosis[] public diagnosisList;
+    DrugPrescription[] public drugPrescription;
+    Surgery[] public surgeries;
+    LaboratoryTest[] public laboratoryTests;
+    BloodDonation[] public bloodDonations;
+    
+    constructor() public {
+        medicalRecordAddress = msg.sender;
+    }
+    
+    function setNationalID(string memory _nationalID) public{
+        nationalID = _nationalID;
+    }
+    
+    function getNationalID() public view returns(string memory){
+        return nationalID;
+    }
+    
+    
+    function setPhoneNumber(string memory _phoneNumber) public{
+        phoneNumber = _phoneNumber;
+    }
+    
+    function getPhoneNumber() public view returns(string memory){
+        return phoneNumber;
+    }
+    
+    function setGender(string memory _gender) public{
+        gender = _gender;
+    }
+    
+    function getGender() public view returns(string memory){
+        return gender;
+    }
+    
+      function setBloodType(string memory _bloodType) public{
+        bloodType = _bloodType;
+    }
+    
+    function getBloodType() public view returns(string memory){
+        return bloodType;
+    }
+    
 
-// TODO: addEmergencyContact
+    function addEmergencyContact(string phoneNumber) public {
+        // TODO: addEmergencyContact
+    }
 
-// TODO: getEmergencyContacts
+    function getEmergencyContacts() public view returns (string[]){
+        // TODO: getEmergencyContacts
+    }
 
-// TODO: addDiognosis
+    function addDiognosis(Diagnosis diagnosis) public{
+        // TODO: addDiognosis
+    }
 
-// TODO: getDiognosis
+    function getDiognosis() public view returns (Diagnosis[]){
+        // TODO: getDiognosis
+    }
 
-// TODO: addDrugPrescription
+    function addDrugPrescription(DrugPrescription drugPrescription) public {
+        // TODO: addDrugPrescription
+    }
 
-// TODO: getDrugPrescription
+    function getDrugPrescription() public view returns (DrugPrescription[]){
+        // TODO: getDrugPrescription
+    }
 
-// TODO: addSurgery
+    function addSurgery(Surgery) public medical{
+      surgeries.push(new Surgery());
+    }
 
-// TODO: getSurgeries
+    function getSurgeries() public medical returns (Surgery){
+       for ( uint i = 0 ; i < surgeries.length ; i++)
+            return surgeries[i];
+    }
 
-// TODO: addLabTest
+    function addLabTest(LaboratoryTest) public{
+        laboratoryTests.push(new LaboratoryTest());
+    }
 
-// TODO: getLabTest
+    function getLabTest() public view returns (LaboratoryTest){
+        for ( uint i = 0 ; i < laboratoryTests.length ; i++)
+            return laboratoryTests[i];
+    }
 
-// TODO: addBloodDonation
+    function addBloodDonation(BloodDonation) public{
+        bloodDonations.push(new BloodDonation());
+    }
 
-// TODO: getBloodDonation
+    function getBloodDonation() public view returns (BloodDonation){
+        for ( uint i = 0 ; i < bloodDonations.length ; i++)
+            return bloodDonations[i];
+    }
 
-// TODO: markTransactionAsCorrection
+    function markTransactionAsCorrection(address Address) public{
+        // TODO: markTransactionAsCorrection
+    }
+    
+      modifier medical() {
+        require(msg.sender == medicalRecordAddress,"Sender not authorized.");
+        _;
+    }
+    
+}
+
+    contract Surgery{
+       
+        string public name;
+        
+        constructor() public{
+            name = "Surgery";
+        }
+        
+    }
+    
+    contract LaboratoryTest{
+        string public name;
+        
+        constructor() public{
+             name = "LaboratoryTest";
+        }
+    }
+    contract BloodDonation{
+       string public name;
+        
+        constructor() public{
+            name = "BloodDonation";
+        }
+    }
+    
+    
+    
+    
