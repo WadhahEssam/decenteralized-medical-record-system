d765f344e261e682626d00d86ec871eda5453227
Wadhah Essam Mac <wadah-wleed@hotmail.com>
2019-02-10 12:38:39 +0300

    adding the test file of the medical record and adding the test for the serguries and their addition

---
+++ test/MedicalRecord.test.js
@@ -0,0 +1,64 @@
+const Web3 = require('web3');
+const ganache = require('ganache-cli');
+const assert = require('assert');
+const compiledMedicalRecordsSystem = require('../build/MedicalRecordsSystem.json');
+const compiledMedicalRecord = require('../build/MedicalRecord.json');
+
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+// var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:7545'));
+
+let accounts, medicalRecordsSystemContract, medicalRecordContract;
+let ministryOfHelath, hospitalOne, hospitalTwo, pharmacyOne, pharmacyTwo;
+
+beforeEach( async () => {
+  accounts = await web3.eth.getAccounts();
+  ministryOfHelath = accounts[0];
+  hospitalOne = accounts[1];
+  hospitalTwo = accounts[2];
+  pharmacyOne = accounts[3];
+  pharmacyTwo = accounts[4];
+
+  medicalRecordsSystemContract = await new web3.eth.Contract(JSON.parse(compiledMedicalRecordsSystem.interface))
+    .deploy({ data: compiledMedicalRecordsSystem.bytecode })
+    .send({ from: ministryOfHelath, gas: '3000000' });
+
+  await medicalRecordsSystemContract.methods.addHospital(hospitalOne, 'king khaled hospital').send({ from: ministryOfHelath, gas: '1000000' });
+  await medicalRecordsSystemContract.methods.createMedicalRecord(425990389, 'Mohammed', 9871634389, '0551292881', 'male', 'o+', '044239448').send({ from: hospitalOne, gas: '2000000' });
+  let medicalRecordAddress = await medicalRecordsSystemContract.methods.getMedicalRecord(425990389).call();
+  medicalRecordContract = await new web3.eth.Contract(
+    JSON.parse(compiledMedicalRecord.interface), 
+    medicalRecordAddress
+  ); 
+});
+
+describe('MedicalRecord Contract', async () => {
+  it('is deployed and has address', async () => {
+    assert.ok(medicalRecordContract.options.address);
+  });
+
+  it('saves the basic information correctly', async () => {
+    assert.equal(await medicalRecordContract.methods.name().call(), 'Mohammed');
+    assert.equal(await medicalRecordContract.methods.birthDate().call(), 9871634389);
+    assert.equal(await medicalRecordContract.methods.phoneNumber().call(), '0551292881');
+    assert.equal(await medicalRecordContract.methods.gender().call(), 'male');
+    assert.equal(await medicalRecordContract.methods.bloodType().call(), 'o+');
+    assert.equal(await medicalRecordContract.methods.emergencyContacts(0).call(), '044239448');
+    assert.equal(await medicalRecordContract.methods.hospitalName().call(), 'king khaled hospital');
+  });
+
+  it('adds a new surgery and stores its data correctly', async () => {
+    await medicalRecordContract.methods.addSurgery('King Khaled Hospital', 'Open Heart Surgery', 'Patient has done an open heart surgery due to a heart disfunctioning', 'Dr. Khaled Al Khateeb', 9834754, 8902, 'x298id02zksoi2083kdx').send({ from: hospitalOne, gas: '2000000' });
+    let surgery = await medicalRecordContract.methods.surgeries(0).call();
+    assert.equal(surgery.hospitalName, 'King Khaled Hospital');
+    assert.equal(surgery.surgeryName, 'Open Heart Surgery');
+    assert.equal(surgery.surgeryDescription, 'Patient has done an open heart surgery due to a heart disfunctioning');
+    assert.equal(surgery.doctor, 'Dr. Khaled Al Khateeb');
+    assert.equal(surgery.date, 9834754);
+    assert.equal(surgery.duration, 8902);
+    assert.equal(surgery.fileHash, 'x298id02zksoi2083kdx');
+    assert.equal(surgery.isMedicalError, false);
+    assert.equal(surgery.isCorrectionFor, '0');
+  });
+
+}); 
